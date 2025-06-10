56ce017e189c1a63cfbde9d2f119acb1a2419b50
piyu989 <sab9861@gmail.com>
2025-06-10 16:41:35 +0530

    IDE

---
+++ src/main/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@SpringBootApplication
+public class DemoApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplicationTests.class, args);
+	}
+	
+	
+}
